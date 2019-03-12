03258b10dac7bc07a5a563b39b8d29bb7bbb739b
Ulrica-11 <634511978#qq.com>
2019-03-12 10:00:40 +0800

    youhua

--- src/todolist/Test.js
+++ src/todolist/Test.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class Test extends Component {
     render() {
+        // 
         // const { content } = this.props;
         return (
             <div>{this.props.content}</div>
